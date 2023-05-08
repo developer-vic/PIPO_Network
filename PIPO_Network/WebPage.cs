a33ead97ecff1763fff4135797329031dd614748
Oluwasegun Babatunde (Programmer G-WIN) <oluwasegunababatunde@gmail.com>
2023-05-08 15:44:15 +0100

    Initial check-in of module PIPO_Network

---
+++ PIPO_Network/WebPage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace PIPO_Network
+{
+    public class WebPage : ContentPage
+    {
+        public WebPage()
+        {
+            var browser = new WebView();
+            browser.Source = "https://lepipo.net/";
+            Content = browser;
+        }
+    }
+}
